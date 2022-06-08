ee6a11552d9cc45e4c895d0e5b61e73b86f793ed
Lucas Gomes <69736708+lkscbrl@users.noreply.github.com>
2022-06-08 11:48:34 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -21,48 +21,85 @@ pages = {'Upload/Download Data':data_upload_download,
          'Analytics':analytics
         }
 
-#st.sidebar.image(image)
-st.sidebar.title('Assessment Toolkit')
-selection = st.sidebar.selectbox('App Navigation', list(pages.keys()))
-page = pages[selection]
-        
-if selection=='Upload/Download Data':
-    sub_sessions = ['Upload Data', 'Inspect Data', 'Download Dataframe']
-    select_df = False
-    
-elif selection=='Data Profiling':
-    select_df = True
-    
-elif selection=='Data Manipulation':
-    sub_sessions = ['New Column', 'Group By', 'Merge Datasets', 'Delete Columns',
-                   'Missing Data', 'Replace Data', 'Filter Data', 'Split Column']
-    select_df = True
-    
-elif selection=='Data Visualization':
-    sub_sessions = ['View as Table', 'Plot Charts']
-    select_df = True
-    
-elif selection=='Analytics':
-    sub_sessions = ['Statistics', 'Crosstable', 'Pairplot', 'Pearson´s Correlation']
-    select_df = True
-    
-elif selection=='Mapping':
-    sub_sessions = ['Set Variables Types', 'Map Interest Variables']
-    select_df = True
 
-if select_df:
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if (
+            st.session_state["username"] in st.secrets["passwords"]
+            and st.session_state["password"]
+            == st.secrets["passwords"][st.session_state["username"]]
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store username + password
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show inputs for username + password.
+        st.text_input("Username", on_change=password_entered, key="username")
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input("Username", on_change=password_entered, key="username")
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 User not known or password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
+
+if check_password():
+
+    st.sidebar.title('Assessment Toolkit')
+    selection = st.sidebar.selectbox('App Navigation', list(pages.keys()))
+    page = pages[selection]
+            
+    if selection=='Upload/Download Data':
+        sub_sessions = ['Upload Data', 'Inspect Data', 'Download Dataframe']
+        select_df = False
+        
+    elif selection=='Data Profiling':
+        select_df = True
+        
+    elif selection=='Data Manipulation':
+        sub_sessions = ['New Column', 'Group By', 'Merge Datasets', 'Delete Columns',
+                       'Missing Data', 'Replace Data', 'Filter Data', 'Split Column']
+        select_df = True
+        
+    elif selection=='Data Visualization':
+        sub_sessions = ['View as Table', 'Plot Charts']
+        select_df = True
+        
+    elif selection=='Analytics':
+        sub_sessions = ['Statistics', 'Crosstable', 'Pairplot', 'Pearson´s Correlation']
+        select_df = True
+        
+    elif selection=='Mapping':
+        sub_sessions = ['Set Variables Types', 'Map Interest Variables']
+        select_df = True
     
-    try:
-        st.session_state['select_df'] = st.sidebar.selectbox('Select Dataset:',
-                                        list(st.session_state['dfs'].keys()))
+    if select_df:
+        
+        try:
+            st.session_state['select_df'] = st.sidebar.selectbox('Select Dataset:',
+                                            list(st.session_state['dfs'].keys()))
+            
+        except:
+            pass
+    
+    try: 
+        st.session_state['sub_session'] = st.sidebar.radio('Sub Session:', sub_sessions)
         
     except:
         pass
-
-try: 
-    st.session_state['sub_session'] = st.sidebar.radio('Sub Session:', sub_sessions)
     
-except:
-    pass
-
-page.app()
+    page.app()
